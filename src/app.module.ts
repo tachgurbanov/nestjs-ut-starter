58e023a530016ee76c7d81c5042ee74c1abc3437
uzunertoprak <toprakuzuner067@gmail.com>
2022-07-10 01:37:29 +0300

    i dont want talk about it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
-import { AuthorizationModule } from './modules/authorization/authorization.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule,
-    AuthorizationModule,
-    UserModule,
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
 })
 export class AppModule {}
